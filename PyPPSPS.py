395c1e4f11bd75ac782e55eeca6109abb8b8f7b7
Nircek <nircek-2103@protonmail.com>
2018-11-19 18:37:33 +0100

    Add headers of functions of PyPPSPS

--- PyPPSPS.py
+++ PyPPSPS.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 class PyPPSPS:
-    pass
+    def __init__(self, server):
+        self.server = server
+    def connect(self):
+        pass
+    def log(msg):
+        pass
+    def pop():
+        pass
+    def reply(user, text):
+        pass
+    def varread(user, name):
+        pass
+    def varwrite(user, name, value):
+        pass
 
 if __name__ == '__main__':
     print('Hello world!')
