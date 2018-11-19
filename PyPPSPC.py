e44b8ab23aebbe35dfe810872fa0ed6902a05360
Nircek <nircek-2103@protonmail.com>
2018-11-19 18:45:04 +0100

    Add headers of functions of PyPPSPC

--- PyPPSPC.py
+++ PyPPSPC.py
@@ -2,6 +2,14 @@
 class PyPPSPC:
     def __init__(self, url):
         self.url = url
+    def register(self, user, password):
+        pass
+    def login(self, user, password):
+        pass
+    def push(self, event):
+        pass
+    def refresh(self):
+        pass
 
 if __name__ == '__main__':
     print('Hello world!')
