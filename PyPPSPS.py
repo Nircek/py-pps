6b2da6b0711f4b613f4337e0600412c427154890
Nircek <nircek-2103@protonmail.com>
2018-11-22 20:05:48 +0100

    Add url() function

--- PyPPSPS.py
+++ PyPPSPS.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+from urllib.request import Request, urlopen
+def url(url):
+  q = Request(url)
+  q.add_header('User-Agent', 'Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405')
+  q.add_header('Cookie', '__test=84955b7c4d6c13ba5eeddd6fcde61533')
+  return urlopen(q).read().decode()
 class PyPPSPS:
     def __init__(self, url, server):
         self.url = url
@@ -17,4 +23,4 @@ class PyPPSPS:
         pass
 
 if __name__ == '__main__':
-    print('Hello world!')
+    print(url('http://pps.rf.gd/pseudophpserver.php'))
