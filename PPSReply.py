80131f1d12a3701079350530a7ca99300843ae0a
Nircek <nircek-2103@protonmail.com>
2019-02-11 19:30:03 +0100

    Add version checking

--- PPSReply.py
+++ PPSReply.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from enum import Enum
 from urllib.request import Request, urlopen
+from urllib.parse import quote_plus
 
 class PPSCodeException(Exception):
     pass
@@ -79,19 +80,30 @@ class PPSReply:
         return 'PPSReply' + repr((self.type, self.args))
 
 def url(uri):
-    q = Request(uri)
-    q.add_header('User-Agent', 'Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405')
-    q.add_header('Cookie', url.cookie)
-    return urlopen(q).read().decode()
+    try:
+        q = Request(uri)
+        q.add_header('User-Agent', 'Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405')
+        q.add_header('Cookie', url.cookie)
+        return urlopen(q).read().decode()
+    except Exception:
+        print(uri)
+        raise
 url.cookie = '__test=eb93faad212ed7dc5ddc4a144182d12d '
 
 def perform(uri, cmd, params=()):
     args='?'
     for k, v in params.items():
-        args += k + '=' + v + '&'
+        args += quote_plus(k) + '=' + quote_plus(v) + '&'
     args = args[:-1]
     return PPSReply(url(uri+'/'+cmd+'.php'+args)).exec()
 
 def inputfunc(title, msg):
     print(title, msg, sep='\n')
     return input()
+
+def boolfunc(title, msg):
+    print(title, str(msg)+' [type \'y\' to agree or \'n\' to disagree and press enter]:', sep='\n')
+    s = ''
+    while not (s == 'y' or s == 'n'):
+        s = input()
+    return True if s == 'y' else False
--- PyPPSPS.py
+++ PyPPSPS.py
@@ -2,32 +2,39 @@
 from PPSReply import *
 
 class PyPPSPS:
+    PPSversion = 'v1.3'
     def __init__(self, url, server):
         self.url = url
         self.server = server
         self.input = inputfunc
+        self.bool = boolfunc
+    def version(self):
+        return url(self.url+'/pseudophpserver.php').rstrip('\r\n')
+    test = lambda self: self.version()[0] == 'v'
     def connect(self):
-        while True:
-            self.version = url(self.url+'/pseudophpserver.php')
-            if self.version[0] != 'v':
-                url.cookie = self.input('Authorization', 'Open ' + self.url+'/cookies.php' + ' and paste here what you get')
-            else:
-                break
+        while not self.test():
+            url.cookie = self.input('Authorization', 'Open ' + self.url+'/cookies.php' + ' and paste here what you get')
+        rv = self.version()
+        if rv != self.PPSversion:
+            if not self.bool('Version mismatch', 'Your version of PPS is \''+self.PPSversion+'\' but version of chosen server\'s PPS is \''
+                             +rv+'\'. Do you want to continue connecting to this server?'):
+                raise SystemExit('User chose not to continue connecting to the server.')
     def log(self, msg):
-        perform(self.url, '', {'server':self.server, 'log': msg})
+        perform(self.url, 'log', {'server':self.server, 'log': msg})
     def pop(self):
-        perform(self.url, '', {'server':self.server})
+        perform(self.url, 'pop', {'server':self.server})
     def reply(self, user, text):
-        perform(self.url, '', {'server':self.server, 'text': text, 'user': user})
+        perform(self.url, 'reply', {'server':self.server, 'text': text, 'user': user})
     def varread(self, user, name):
-        perform(self.url, '', {'server':self.server, 'name': name, 'user': user})
+        perform(self.url, 'varread', {'server':self.server, 'name': name, 'user': user})
     def varwrite(self, user, name, value):
-        perform(self.url, '', {'server':self.server, 'name': name, 'user': user, 'value': value})
+        perform(self.url, 'varwrite', {'server':self.server, 'name': name, 'user': user, 'value': value})
 
 def main():
     p = PyPPSPS('http://pps.rf.gd', 'test')
     p.connect()
-    print(p.version)
+    print(p.version())
+    p.log('Hello from Python!')
 
 
 if __name__ == '__main__':
