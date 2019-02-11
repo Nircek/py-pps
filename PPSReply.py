074450332a172405f3449f4afc5c582678079bc4
Nircek <nircek-2103@protonmail.com>
2019-02-11 18:31:32 +0100

    Move common functions for PS and PC to PPSReply

--- PPSReply.py
+++ PPSReply.py
@@ -76,3 +76,14 @@ class PPSReply:
             raise c(self.args)
     def __repr__(self):
         return 'PPSReply' + repr((self.type, self.args))
+
+def url(uri):
+    q = Request(uri)
+    q.add_header('User-Agent', 'Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405')
+    q.add_header('Cookie', url.cookie)
+    return urlopen(q).read().decode()
+url.cookie = '__test=eb93faad212ed7dc5ddc4a144182d12d '
+
+def inputfunc(title, msg):
+    print(title, msg, sep='\n')
+    return input()
