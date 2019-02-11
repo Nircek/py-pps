dcd8e6154fe4d029c69dfa7e4a6fc3dd2e24dd5b
Nircek <nircek-2103@protonmail.com>
2019-02-11 18:19:21 +0100

    Add the PPSCodeException structure

--- PPSReply.py
+++ PPSReply.py
@@ -1,6 +1,47 @@
 #!/usr/bin/env python3
 from enum import Enum
 
+class PPSCodeException(Exception):
+    pass
+
+class PPSError(PPSCodeException):
+    pass
+
+class PPSDBError(PPSCodeException):
+    pass
+
+class PPSUnexpectedError(PPSCodeException):
+    pass
+
+'''
+1   Not enough parameters in the request
+2   User privileges denied
+3   Admin privileges denied
+4   Log file problem
+5   No elements in the queue
+6   A user with the same login exists
+'''
+class PPSParameterError(PPSError):
+    pass
+
+class PPSUserDeniedError(PPSError):
+    pass
+
+class PPSAdminDeniedError(PPSError):
+    pass
+
+class PPSLogError(PPSError):
+    pass
+
+class PPSNullError(PPSError):
+    pass
+
+class PPSSameUserError(PPSError):
+    pass
+
+class PPSUnsupportedError(PPSError):
+    pass
+
 class PPSCode(Enum):
     GOOD = 0
     ERROR = 1
@@ -8,10 +49,7 @@ class PPSCode(Enum):
     UNEXPECTEDERROR = 3
     def get(s):
         d = {'0': PPSCode.GOOD, '-': PPSCode.ERROR, '+': PPSCode.DBERROR}
-        if len(s) == 0 or not s[0] in d:
-            return PPSCode.UNEXPECTEDERROR
-        else:
-            return d[s[0]]
+        return PPSCode.UNEXPECTEDERROR if len(s) == 0 or not s[0] in d else d[s[0]]
     def __bool__(self):
         return self == PPSCode.GOOD
 
