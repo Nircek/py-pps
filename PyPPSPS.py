f1c16f3c7f9abcd97f28fa2e58d0fa301c6d2b56
Nircek <nircek-2103@protonmail.com>
2019-02-11 15:39:26 +0100

    Add PPSCode

--- PyPPSPS.py
+++ PyPPSPS.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
 from urllib.request import Request, urlopen
+from enum import Enum
 def url(uri):
     q = Request(uri)
     q.add_header('User-Agent', 'Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405')
     q.add_header('Cookie', url.cookie)
     return urlopen(q).read().decode()
 url.cookie = '__test=eb93faad212ed7dc5ddc4a144182d12d '
+
+class PPSCode(Enum):
+    GOOD = 0
+    ERROR = 1
+    DBERROR = 2
+    UNEXPECTEDERROR = 3
+    def get(s):
+        d = {'0': PPSCode.GOOD, '-': PPSCode.ERROR, '+': PPSCode.DBERROR}
+        if len(s) == 0 or not s[0] in d:
+            return PPSCode.UNEXPECTEDERROR
+        else:
+            return d[s[0]]
 
 class PyPPSPS:
     def __init__(self, url, server):
