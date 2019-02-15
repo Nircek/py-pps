a6196a4de6671a2894f7759b80e4b82ec9812a8f
Nircek <nircek-2103@protonmail.com>
2019-02-15 17:51:57 +0100

    Add sending of data via POST

--- PPSDefaults.py
+++ PPSDefaults.py
@@ -1,26 +1,22 @@
 #!/usr/bin/env python3
 from urllib.request import Request, urlopen
-from urllib.parse import quote_plus
+from urllib.parse import quote_plus, urlencode
 from .PPSReply import *
 import warnings
 
-def url(uri):
+def url(uri, data={}):
     try:
         q = Request(uri)
         q.add_header('User-Agent', 'Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405')
         q.add_header('Cookie', url.cookie)
-        return urlopen(q).read().decode()
+        return urlopen(q, data=urlencode(data).encode()).read().decode()
     except Exception:
         print(uri)
         raise
 url.cookie = '__test=c3495eae2f165b2aa28f8d430d413014 '
 
-def perform(uri, cmd, params=()):
-    args='?'
-    for k, v in params.items():
-        args += quote_plus(k) + '=' + quote_plus(v) + '&'
-    args = args[:-1]
-    return PPSReply(url(uri+'/'+cmd+'.php'+args)).exec()
+def perform(uri, cmd, params={}):
+    return PPSReply(url(uri+'/'+cmd+'.php', params)).exec()
 
 getVersion = lambda uri: url(uri+'/pseudophpserver.php').rstrip('\r\n')
 
